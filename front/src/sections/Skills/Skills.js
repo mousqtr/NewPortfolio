a2f7f584ff62e124edf7949c9236405696214038
mousQTR <49991720+mousqtr@users.noreply.github.com>
2023-09-28 13:07:59 +0000

    Update 3 files

--- front/src/sections/Skills/Skills.js
+++ front/src/sections/Skills/Skills.js
@@ -1,7 +1,26 @@
 import "./Skills.scss";
 
 function Skills() {
-  return <div id="skills" className="app-section"></div>;
+  return (
+    <div id="skills" className="app-section">
+      <div className="block">
+        <h1>Langages de programmation</h1>
+        <div className="cols">
+          <div className="column">Javascript</div>
+          <div className="column">PHP</div>
+          <div className="column">Python</div>
+        </div>
+      </div>
+      <div className="block">
+        <h1>Frameworks & Librairies</h1>
+        <div className="cols">
+          <div className="column">React.js</div>
+          <div className="column">Node.js</div>
+          <div className="column">Angular</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
