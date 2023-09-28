73c96ce57f138fe358ac53b895be2c896b1adce7
mousQTR <49991720+mousqtr@users.noreply.github.com>
2023-09-28 10:46:23 +0000

    Update 11 files

--- front/src/sections/Presentation/Presentation.js
+++ front/src/sections/Presentation/Presentation.js
@@ -1,7 +1,24 @@
 import "./Presentation.scss";
 
 function Presentation() {
-  return <div id="presentation" className="app-section"></div>;
+  return (
+    <div id="presentation" className="app-section">
+      <div className="block block-left">
+        <div className="presentation-text">
+          <h1>Mustapha BENBRIKHO</h1>
+          <h2>Ingénieur Développeur Full Stack</h2>
+          <p>
+            Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à
+            la bureautique informatique, sans que son contenu n'en soit modifié.
+            Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à
+            la bureautique informatique, sans que son contenu n'en soit modifié.
+          </p>
+          <button className="btn btn-classic">Me contacter</button>
+        </div>
+      </div>
+      <div className="block block-right"></div>
+    </div>
+  );
 }
 
 export default Presentation;
