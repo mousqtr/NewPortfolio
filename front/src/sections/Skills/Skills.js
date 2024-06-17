df6c274e2d258817e66e284b3e22e079b8060904
mousQTR <49991720+mousqtr@users.noreply.github.com>
2024-06-17 13:10:09 +0000

    Add icon technos

--- front/src/sections/Skills/Skills.js
+++ front/src/sections/Skills/Skills.js
@@ -3,36 +3,36 @@ import "./Skills.scss";
 
 const dictionnary = {
   languages: {
-    html: { label: "Javascript", logo: "" },
-    css: { label: "CSS", logo: "" },
-    javascript: { label: "Javascript", logo: "" },
-    php: { label: "PHP", logo: "" },
-    python: { label: "Python", logo: "" },
+    html: { label: "Javascript", logo: "react.png" },
+    css: { label: "CSS", logo: "react.png" },
+    javascript: { label: "Javascript", logo: "react.png" },
+    php: { label: "PHP", logo: "react.png" },
+    python: { label: "Python", logo: "react.png" },
   },
   libraries: {
-    react_js: { label: "React.js", logo: "" },
-    angular: { label: "Angular", logo: "" },
-    node_js: { label: "Node.js", logo: "" },
+    react_js: { label: "React.js", logo: "react.png" },
+    angular: { label: "Angular", logo: "react.png" },
+    node_js: { label: "Node.js", logo: "react.png" },
   },
   databases: {
-    mongodb: { label: "MongoDB", logo: "" },
-    postgresql: { label: "PostgreSQL", logo: "" },
-    mysql: { label: "MySQL", logo: "" },
+    mongodb: { label: "MongoDB", logo: "react.png" },
+    postgresql: { label: "PostgreSQL", logo: "react.png" },
+    mysql: { label: "MySQL", logo: "react.png" },
   },
   tools: {
-    vscode: { label: "Visual Studio Code", logo: "" },
+    vscode: { label: "Visual Studio Code", logo: "react.png" },
   },
 };
 
 const data = {
-  langages: ["html", "css", "javascript", "python"],
+  languages: ["html", "css", "javascript", "python"],
   libraries: ["react_js", "angular"],
   databases: ["mongodb"],
   tools: ["vscode"],
 };
 
 const parts = {
-  langages: { title: "Langages de programmation" },
+  languages: { title: "Langages de programmation" },
   libraries: { title: "Frameworks & Librairies" },
   databases: { title: "Bases de données" },
   tools: { title: "Outils" },
@@ -44,11 +44,18 @@ function Skills() {
       <div className="main-block">
         <h1>Boite à outils</h1>
         {Object.keys(parts).map((id) => (
-          <div className="sub-block">
+          <div key={id} className="sub-block">
             <h2>{parts[id].title}</h2>
             <ul>
               {data[id].map((elt) => (
-                <li></li>
+                <li key={elt}>
+                  <img
+                    src={`/img/skills/libraries/react.png`}
+                    // src={`/img/skills/${id}/${dictionnary[id][elt].logo}`}
+                    alt="profile-image"
+                  />
+                  <div className="elt-title">{dictionnary[id][elt].label}</div>
+                </li>
               ))}
             </ul>
           </div>
