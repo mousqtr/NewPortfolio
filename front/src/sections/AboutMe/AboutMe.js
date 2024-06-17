d558ecb4422fe537cac3f958bda9de7e8ef1b4f2
mousQTR <49991720+mousqtr@users.noreply.github.com>
2024-06-17 11:22:49 +0000

    Save progress

--- front/src/sections/AboutMe/AboutMe.js
+++ front/src/sections/AboutMe/AboutMe.js
@@ -4,7 +4,19 @@ export default function AboutMe() {
   return (
     <div id="aboutMe" className="app-section">
       <div className="main-block">
-        <h1>A propos de moi</h1>
+        <div className="sub-block block-left">
+          <img src="/img/about.jpg" alt="profile-image" />
+        </div>
+        <div className="sub-block block-right">
+          <h1>A propos de moi</h1>
+          <p>
+            Le Lorem Ipsum est simplement du faux texte employé dans la
+            composition et la mise en page avant impression. Le Lorem Ipsum est
+            le faux texte standard de l'imprimerie depuis les années 1500, quand
+            un imprimeur anonyme assembla ensemble des morceaux de texte pour
+            réaliser un livre spécimen de polices de texte.
+          </p>
+        </div>
       </div>
     </div>
   );
--- front/src/sections/Skills/Skills.js
+++ front/src/sections/Skills/Skills.js
@@ -1,67 +1,58 @@
 import { useState, useMemo } from "react";
-import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import "./Skills.scss";
 
-const levels = {
-  senior: "Senior",
-  intermediate: "Intermédiaire",
-  junior: "Junior",
+const dictionnary = {
+  languages: {
+    html: { label: "Javascript", logo: "" },
+    css: { label: "CSS", logo: "" },
+    javascript: { label: "Javascript", logo: "" },
+    php: { label: "PHP", logo: "" },
+    python: { label: "Python", logo: "" },
+  },
+  libraries: {
+    react_js: { label: "React.js", logo: "" },
+    angular: { label: "Angular", logo: "" },
+    node_js: { label: "Node.js", logo: "" },
+  },
+  databases: {
+    mongodb: { label: "MongoDB", logo: "" },
+    postgresql: { label: "PostgreSQL", logo: "" },
+    mysql: { label: "MySQL", logo: "" },
+  },
+  tools: {
+    vscode: { label: "Visual Studio Code", logo: "" },
+  },
 };
-const languages = [
-  { label: "Javascript", logo: "", level: "senior" },
-  { label: "PHP", logo: "", level: "senior" },
-  { label: "HTML/CSS/SCSS", logo: "", level: "senior" },
-  { label: "Python", logo: "", level: "intermediate" },
-  { label: "C++", logo: "", level: "intermediate" },
-];
 
-const libraries = [
-  { label: "React.js", logo: "", level: "senior" },
-  { label: "Node.js", logo: "", level: "senior" },
-  { label: "Angular", logo: "", level: "senior" },
-];
+const data = {
+  langages: ["html", "css", "javascript", "python"],
+  libraries: ["react_js", "angular"],
+  databases: ["mongodb"],
+  tools: ["vscode"],
+};
+
+const parts = {
+  langages: { title: "Langages de programmation" },
+  libraries: { title: "Frameworks & Librairies" },
+  databases: { title: "Bases de données" },
+  tools: { title: "Outils" },
+};
 
 function Skills() {
-  const [languagePage, setLanguagePage] = useState(1);
-  const [libraryPage, setLibraryPage] = useState(1);
-
-  const displayedLanguages = useMemo(() => {
-    return languages.slice(0, languagePage * 3);
-  }, [languagePage]);
-
-  const displayedLibraries = useMemo(() => {
-    return libraries.slice(0, libraryPage * 3);
-  }, [libraryPage]);
-
   return (
     <div id="skills" className="app-section">
-      <div className="block">
-        <h1>Langages de programmation</h1>
-        <div className="cols">
-          <AiOutlineLeft />
-          {displayedLanguages.map((language, index) => {
-            return (
-              <div key={index} className="column">
-                {language.label}
-              </div>
-            );
-          })}
-          <AiOutlineRight />
-        </div>
-      </div>
-      <div className="block">
-        <h1>Frameworks & Librairies</h1>
-        <div className="cols">
-          <AiOutlineLeft />
-          {displayedLibraries.map((library, index) => {
-            return (
-              <div key={index} className="column">
-                {library.label}
-              </div>
-            );
-          })}
-          <AiOutlineRight />
-        </div>
+      <div className="main-block">
+        <h1>Boite à outils</h1>
+        {Object.keys(parts).map((id) => (
+          <div className="sub-block">
+            <h2>{parts[id].title}</h2>
+            <ul>
+              {data[id].map((elt) => (
+                <li></li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </div>
   );
