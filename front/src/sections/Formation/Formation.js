d21d6d9072a7415052313d601a46a58eb4eeec8b
mousQTR <49991720+mousqtr@users.noreply.github.com>
2024-06-18 11:55:50 +0000

    Add formation section

--- front/src/sections/Formation/Formation.js
+++ front/src/sections/Formation/Formation.js
@@ -1,7 +1,53 @@
 import "./Formation.scss";
 
+const data = [
+  {
+    order: 1,
+    image: "/img/about.jpg",
+    title: "Lycée Henri IV",
+    description: `Le Lorem Ipsum est simplement du faux texte employé dans la
+    composition et la mise en page avant impression.`,
+  },
+  {
+    order: 2,
+    image: "/img/about.jpg",
+    title: "Lycée Henri IV",
+    description: `Le Lorem Ipsum est simplement du faux texte employé dans la
+    composition et la mise en page avant impression.`,
+  },
+  {
+    order: 2,
+    image: "/img/about.jpg",
+    title: "Lycée Henri IV",
+    description: `Le Lorem Ipsum est simplement du faux texte employé dans la
+    composition et la mise en page avant impression.`,
+  },
+  {
+    order: 2,
+    image: "/img/about.jpg",
+    title: "Lycée Henri IV",
+    description: `Le Lorem Ipsum est simplement du faux texte employé dans la
+    composition et la mise en page avant impression.`,
+  },
+];
+
 function Formation() {
-  return <div id="formation" className="app-section"></div>;
+  return (
+    <div id="formation" className="app-section">
+      <div className="main-block">
+        <h1>Formation</h1>
+        <ul>
+          {data.map((formation, index) => (
+            <li key={index}>
+              <img src={formation.image} alt="formation-image" />
+              <h2>{formation.title}</h2>
+              <p>{formation.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Formation;
